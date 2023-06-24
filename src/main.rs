34a73e7ff756a89b31f716008ae11f28926da789
Samuel Henderson <samzi.henderson@gmail.com>
2023-06-23 23:05:50 -0700

    add lichess API calls to accept challenges and make moves

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,205 @@ use pleco::{Board,Player,PieceType,SQ,BitMove};
 use std::cmp::max;
 use std::cmp::min;
 
-#[allow(dead_code)]
-fn main() {
-    let board = Board::start_pos();
-    //let board = Board::from_fen("rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2").unwrap();
+use dotenv::dotenv;
+use std::env;
 
-    // draw_board(board.clone());
-    // let next_move = minimax(board.clone(), 1);
+use reqwest::Error;
+use std::time::Duration;
 
-    // println!("{}", next_move);
+use serde_json::Value;
 
-    play_against_itself(board.clone(), 5);
+const NUM_TURNS_TO_LOOK_AHEAD: u8 = 3;
+
+// #[allow(dead_code)]
+// #[tokio::main]
+// async fn main() {
+//     let board = Board::start_pos();
+//     play_against_itself(board.clone(), 3);
+// }
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    dotenv().ok();
+    //respond_to_all_challenges().await?;
+    make_moves_in_all_ongoing_games().await?;
+
+    Ok(())
 }
 
+// finds all current games of the bot and makes a move in those where it is the bot's turn
+async fn make_moves_in_all_ongoing_games() -> Result<(), Error> {
+    let LICHESS_ACCESS_TOKEN = env::var("RUST_BOT_TOKEN").expect("You need to set a Lichess personal access token.");
+
+    let ongoing_games = get_ongoing_games().await?;
+    println!("ongoing_games: {}", ongoing_games);
+    let ongoing_games_object: Value = serde_json::from_str(&ongoing_games).unwrap();
+    let game_array_json = &ongoing_games_object["nowPlaying"];
+
+    if game_array_json == &Value::Null && game_array_json == &Value::Array(vec![]) {
+        println!("No ongoing games");
+        return Ok(());
+    }
+
+    // loop through all current games and make a move in each
+    if let Some(games) = game_array_json.as_array() {
+        for game in games {
+            let game_id = &game["gameId"].as_str().unwrap();
+            let fen = &game["fen"].as_str().unwrap();
+            let is_my_turn = &game["isMyTurn"].as_bool().unwrap();
+
+            if *is_my_turn == true {
+                let board = Board::from_fen(fen).unwrap();
+                let next_move = calculate_next_move(board);
+                println!("making move: {}", next_move);
+
+                make_move(&game_id, &next_move).await?;
+            }
+        }
+    }
+
+    Ok(())
+}
+
+// calls API to make a move in the given game. Move should be a string in UCI format, e.g. "e2e4"
+async fn make_move(game_id: &str, move_to_play: &str) -> Result<String, Error> {
+    let LICHESS_ACCESS_TOKEN = env::var("RUST_BOT_TOKEN").expect("You need to set a Lichess personal access token.");
+    let URL = "https://lichess.org/api/bot/game/".to_owned() + game_id + "/move/" + move_to_play;
+
+    let client = reqwest::Client::new();
+    let response = client
+        .post(URL)
+        .header("Authorization", "Bearer ".to_owned() + LICHESS_ACCESS_TOKEN.as_str())
+        .timeout(Duration::from_secs(5))
+        .send()
+        .await?
+        .text()
+        .await?;
+
+    println!("response after making a move: {}", response);
+
+    Ok(response)
+}
+
+// fetches all current challenges for the bot, and accepts all that are at least 1 day per turn. It declines the rest
+async fn respond_to_all_challenges() -> Result<(), Error> {
+    let LICHESS_ACCESS_TOKEN = env::var("RUST_BOT_TOKEN").expect("You need to set a Lichess personal access token.");
+
+    let current_challenges = list_current_challenges().await?;
+    let challenges_object: Value = serde_json::from_str(&current_challenges).unwrap();
+    
+    let challenges_received = &challenges_object["in"];
+    if challenges_received == &Value::Null && challenges_received == &Value::Array(vec![]) {
+        println!("No challenges received");
+        return Ok(());
+    }
+
+    // loop through challenges_received and accept all that are at least 1 day per turn
+    if let Some(challenges) = challenges_received.as_array() {
+        for challenge in challenges {
+            let challenge_id = &challenge["id"].as_str().unwrap();
+            println!("challenge_id: {}", challenge_id);
+
+            let days_per_turn = &challenge["timeControl"]["daysPerTurn"];
+            println!("days_per_turn: {}", days_per_turn);
+            
+            if days_per_turn == &Value::Null {
+                // decline challenge as only accepting games with at least 1 day per turn
+                println!("declining challenge");
+                decline_challenge(challenge_id).await?;
+            }
+            else {
+                // accept challenge
+                println!("accepting challenge - need to re-enable");
+                accept_challenge(challenge_id).await?;
+            }
+        }
+    }
+
+    Ok(())
+}
+
+// fetches all ongoing games for the bot
+async fn get_ongoing_games() -> Result<String, Error> {
+    let LICHESS_ACCESS_TOKEN = env::var("RUST_BOT_TOKEN").expect("You need to set a Lichess personal access token.");
+
+    let client = reqwest::Client::new();
+    let response = client
+        .get("https://lichess.org/api/account/playing")
+        .header("Authorization", "Bearer ".to_owned() + LICHESS_ACCESS_TOKEN.as_str())
+        .timeout(Duration::from_secs(5))
+        .send()
+        .await?
+        .text()
+        .await?;
+
+    Ok(response)
+}
+
+// accepts a challenge with the given challenge ID
+async fn accept_challenge(challenge_id: &str) -> Result<(), Error> {
+    let LICHESS_ACCESS_TOKEN = env::var("RUST_BOT_TOKEN").expect("You need to set a Lichess personal access token.");
+    let URL = "https://lichess.org/api/challenge/".to_owned() + challenge_id + "/accept";
+    println!("URL: {}", URL);
+
+    let client = reqwest::Client::new();
+    let response = client
+        .post(URL)
+        .header("Authorization", "Bearer ".to_owned() + LICHESS_ACCESS_TOKEN.as_str())
+        .timeout(Duration::from_secs(5))
+        .send()
+        .await?
+        .text()
+        .await?;
+
+    println!("response: {}", response);
+
+    Ok(())
+}
+
+// declines a challenge with the given challenge ID
+async fn decline_challenge(challenge_id: &str) -> Result<(), Error> {
+    let LICHESS_ACCESS_TOKEN = env::var("RUST_BOT_TOKEN").expect("You need to set a Lichess personal access token.");
+    let URL = "https://lichess.org/api/challenge/".to_owned() + challenge_id + "/decline";
+    println!("URL: {}", URL);
+
+    let client = reqwest::Client::new();
+    let response = client
+        .post(URL)
+        .header("Authorization", "Bearer ".to_owned() + LICHESS_ACCESS_TOKEN.as_str())
+        .body("declineTooFast")
+        .timeout(Duration::from_secs(5))
+        .send()
+        .await?
+        .text()
+        .await?;
+
+    Ok(())
+}
+
+async fn list_current_challenges() -> Result<String, Error> {
+    let LICHESS_ACCESS_TOKEN = env::var("RUST_BOT_TOKEN").expect("You need to set a Lichess personal access token.");
+
+    let client = reqwest::Client::new();
+    let response = client
+        .get("https://lichess.org/api/challenge")
+        .header("Authorization", "Bearer ".to_owned() + LICHESS_ACCESS_TOKEN.as_str())
+        .timeout(Duration::from_secs(5))
+        .send()
+        .await?
+        .text()
+        .await?;
+
+    Ok(response)
+}
+
+// returns the best move as a string for the current player in the given board state
+fn calculate_next_move(mut board: Board) -> String {
+    let next_move = minimax(board.clone(), NUM_TURNS_TO_LOOK_AHEAD);
+    next_move.to_string().parse().unwrap()
+}
+
+// call to make the bot play against itself. Used for testing
 fn play_against_itself(mut board: Board, depth: u8) {
     while board.checkmate() == false && board.stalemate() == false {
         let next_move = minimax(board.clone(), depth);
@@ -40,7 +226,7 @@ fn play_against_itself(mut board: Board, depth: u8) {
     }
 }
 
-//takes in a board and returns the best move to make
+// takes in a board and returns the best move to make for the current player
 fn minimax(mut board: Board, depth: u8) -> BitMove {
     let possible_moves = board.generate_moves();
     let mut alpha = -9999999;
